2b27463187cd56bef9578712d821f732fc1280a3
Prota <protez95@gmail.com>
2020-02-02 15:49:42 +0100

    Initial commit

---
+++ Assets/Scripts/GrunwaldController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrunwaldController : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void AudioPlay(AudioClip otherClip)
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.clip = otherClip;
+            audioSource.Play();
+        }
+    }
+
+}
